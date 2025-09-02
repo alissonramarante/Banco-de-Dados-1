6066f29721bdd8c86e708a418dbb23ec1bd8e3e7
eduardolacerdajr <97895745+eduardolacerdajr@users.noreply.github.com>
2025-09-02 16:59:08 -0300

    Questões 4 e 5 Fase 3.sql

--- Fase 3.sql
+++ Fase 3.sql
@@ -12,7 +12,35 @@ SELECT DISTINCT c.NOME
 --2 - Qual(is) o(s) produto(s) mais bem avaliado(s) (com a maior média de avaliação) pelos clientes de sobrenome ‘Medeiros’? Projete o código e nome do(s) produto(s).
 --3 - Exiba o nome, sobrenome, email e código dos clientes que possuem um valor médio de compra superior ou igual a R$ 125,00 e que tenham realizado alguma compra nos últimos 3 meses.
 --4 - Exiba o nome e e-mail de todos os clientes que moram em Belém. Se o cliente já comprou o produto ‘Mesa de madeira’, exiba a quantidade comprada deste produto feita pelo cliente, se não comprou, exiba 0.
+SELECT 
+    c.NOME, 
+    c.SOBRENOME, 
+    c.EMAIL,
+    NVL((
+        SELECT SUM(cpp.QUANTIDADE)
+        FROM COMPRA_POSSUI_PRODUTO cpp
+        JOIN ORDEM_DE_COMPRA oc ON cpp.CODIGO_COMPRA = oc.CODIGO
+        JOIN PRODUTO p ON cpp.CODIGO_PRODUTO = p.CODIGO
+        WHERE oc.CODIGO_CLIENTE = c.CODIGO
+        AND p.NOME = 'Mesa de madeira'
+    ), 0) as QUANTIDADE_COMPRADA
+FROM CLIENTE c
+WHERE c.CIDADE = 'Belém';
+
 --5 - Liste a quantidade de produtos em estoque agrupados por categoria daqueles produtos que já foram comprados pelo menos uma vez.
+SELECT 
+    cat.NOME as CATEGORIA,
+    SUM(ped.QUANTIDADE) as QUANTIDADE_ESTOQUE
+FROM CATEGORIA cat
+JOIN PRODUTO p ON cat.CODIGO = p.CODIGO_CATEGORIA
+JOIN PROD_ESTOCADO_CENT_DIST ped ON p.CODIGO = ped.CODIGO_PRODUTO
+WHERE p.CODIGO IN (
+    SELECT DISTINCT cpp.CODIGO_PRODUTO
+    FROM COMPRA_POSSUI_PRODUTO cpp
+)
+GROUP BY cat.NOME
+ORDER BY cat.NOME;
+
 --6 - Exiba o nome, sobrenome, e-mail e código dos clientes que possuem um valor médio de compra superior ou igual a R$250,00 e que não realizaram compras em 2025.
 --7 - Dentre os clientes que residem em Maceió e gastaram mais de R$5000,00 em compras entre Janeiro 2024 e Março 2025. Exiba o código, nome e valor total gasto em compras no período, ordenado pelo valor total gasto em ordem decrescente.
 --8 - Projeto o nome de cada cidade e o total de clientes que ela possui.
